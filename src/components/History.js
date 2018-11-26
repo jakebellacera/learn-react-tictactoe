13c71d6d5c6f3188c4ff75c79ae8f8e0f1ce5cf7
Jake Bellacera <jake.bellacera@gmail.com>
2018-11-25 19:54:45 -0800

    Convert History to a function component

--- src/components/History.js
+++ src/components/History.js
@@ -1,61 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { SORT_ORDERS } from "../lib/constants";
-import cn from 'classnames';
+import MoveList from "./History/MoveList";
 
-class History extends React.PureComponent {
-  // Returns the col / row for a square
-  getSquareColRow(i) {
-    const col = i % 3;
-    const row = Math.ceil(i / 3);
-    return [col === 0 ? 3 : col, row];
-  }
-
-  render() {
-    let moves = this.props.moves.map((step, move) => {
-      const desc = move ?
-        `Go to move #${move} (${move % 2 !== 0 ? 'X' : 'O'}: ${this.getSquareColRow(step.square + 1).join(',')})` :
-        'Go to game start';
-
-      const classNames = cn({
-        current: move === this.props.currentStep
-      });
-
-      return (
-        <li
-          key={move}
-          className={classNames}
-        >
-          <button onClick={() => this.props.onMoveChange(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    if (this.props.sortOrder === SORT_ORDERS.DESC) {
-      moves = moves.reverse();
-    }
-
-    return (
-      <div className="history">
-        <div className="history-sort">
-          Sort history: 
-          <select
-            value={this.props.sortOrder}
-            onChange={(event) => this.props.onSortOrderChange(event.target.value)}
-          >
-            <option value={SORT_ORDERS.ASC}>ASC</option>
-            <option value={SORT_ORDERS.DESC}>DESC</option>
-          </select>
-        </div>
-        <ol className="history-moves">{moves}</ol>
-      </div>
-    );
-  }
-}
+const History = ({moves, currentStep, sortOrder, onSortOrderChange, onMoveChange}) => (
+  <div className="history">
+    <div className="history-sort">
+      Sort history: 
+      <select
+        value={sortOrder}
+        onChange={(event) => onSortOrderChange(event.target.value)}
+      >
+        <option value={SORT_ORDERS.ASC}>ASC</option>
+        <option value={SORT_ORDERS.DESC}>DESC</option>
+      </select>
+    </div>
+    <MoveList
+      moves={moves}
+      sortOrder={sortOrder}
+      currentStep={currentStep}
+      onMoveChange={onMoveChange}
+    /> 
+  </div>
+);
 
 History.propTypes = {
-  currentStep: PropTypes.number.isRequired,
   moves: PropTypes.arrayOf(PropTypes.object).isRequired,
+  currentStep: PropTypes.number.isRequired,
   sortOrder: PropTypes.oneOf(Object.values(SORT_ORDERS)).isRequired,
   onSortOrderChange: PropTypes.func,
   onMoveChange: PropTypes.func
---
+++ src/components/History/MoveList.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { SORT_ORDERS } from "../../lib/constants";
+import cn from 'classnames';
+
+const MoveList = ({moves, currentStep, onMoveChange, sortOrder}) => {
+  let moveList = moves.map((step, move) => {
+    const desc = move ?
+      `Go to move #${move} (${move % 2 !== 0 ? 'X' : 'O'}: ${getSquareColRow(step.square + 1).join(',')})` :
+      'Go to game start';
+
+    return (
+      <li
+        key={move}
+        className={cn({ current: move === currentStep })}
+      >
+        <button onClick={() => onMoveChange(move)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
+  if (sortOrder === SORT_ORDERS.DESC) {
+    moveList = moveList.reverse();
+  }
+
+  return (
+    <ol className="history-moves">
+      {moveList}
+    </ol>
+  );
+};
+
+MoveList.propTypes = {
+  moves: PropTypes.arrayOf(PropTypes.object).isRequired,
+  currentStep: PropTypes.number.isRequired,
+  onMoveChange: PropTypes.func,
+  sortOrder: PropTypes.oneOf(Object.values(SORT_ORDERS)).isRequired
+};
+
+const getSquareColRow = (i) => {
+  const col = i % 3;
+  const row = Math.ceil(i / 3);
+  return [col === 0 ? 3 : col, row];
+};
+
+export default MoveList;
